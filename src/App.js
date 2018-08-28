78a72fe05e74f3f863eb4982520e6542d380d282
Min Suk Kim <minkimcodes@gmail.com>
2018-08-28 14:57:31 -0400

    react bootstrap properly installed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Nav from './components/Nav';
+import Header from './components/Header';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Nav></Nav>
+        <Header></Header>
       </div>
     );
   }
