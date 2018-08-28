87baf34b184039f01f989755ca5a6a69c733898a
Min Suk Kim <minkimcodes@gmail.com>
2018-08-28 15:06:45 -0400

    nav bar links cleanup

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,25 +14,10 @@ class Header extends Component {
           <Navbar.Collapse>
             <Nav>
               <NavItem eventKey={1} href="#">
-                Link
+                About Min
               </NavItem>
               <NavItem eventKey={2} href="#">
-                Link
-              </NavItem>
-              <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                <MenuItem eventKey={3.1}>Action</MenuItem>
-                <MenuItem eventKey={3.2}>Another action</MenuItem>
-                <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                <MenuItem divider />
-                <MenuItem eventKey={3.3}>Separated link</MenuItem>
-              </NavDropdown>
-            </Nav>
-            <Nav pullRight>
-              <NavItem eventKey={1} href="#">
-                Link Right
-              </NavItem>
-              <NavItem eventKey={2} href="#">
-                Link Right
+                Projects
               </NavItem>
             </Nav>
           </Navbar.Collapse>
