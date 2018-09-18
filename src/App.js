0f8b87593ff8a6c146c35a255b8a79f40c6ae966
Min Suk Kim <minkimcodes@gmail.com>
2018-09-18 18:17:16 -0400

    remove header for now

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Box from './components/Box';
+import './styles.css';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header></Header>
-        <Box></Box>
+        <div className="container">
+          <Box></Box>
+          <Box></Box>
+        </div>
       </div>
     );
   }
--- src/components/Box.js
+++ src/components/Box.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import '../styles.css';
 
 class Box extends React.Component {
   render () {
     return(
-      <div>
+      <div className="box">
         Box!
       </div>
     )
